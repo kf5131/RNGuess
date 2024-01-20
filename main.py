132e95a8fcee1a5a1964f25db22c7ef263e22abb
kf5131 <kf5131@icloud.com>
2024-01-20 19:17:04 +0100

    add main

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import random
 
+def gameloop(number: int):
+    
+    while True:
+        
+        guess = int(input("Guess a number between 1 and 100: "))
+        
+        if guess == number:
+            print("You guessed it!")
+            break
+        elif guess < number:
+            print("Too low!")
+        else:
+            print("Too high!")
+    
 
-
+def main():
+    
+    number = random.randint(1, 100)
+    
+    gameloop(number)
+    
+    
+if __name__ == "__main__":
+    main()
